59133398195f5214803898a15a3c354391be404a
Starsick007 <60711605+Starsick007@users.noreply.github.com>
2020-02-05 14:00:48 -0500

    Update HACKFB.py

--- HACKFB.py
+++ HACKFB.py
@@ -17,7 +17,7 @@ logo = """\x1b[34m
                   ░                            ░\x1b[00m"""
 
 banner = """
-\x1b[Author : Mr.Starsick
+\x1b[34mAuthor : Mr.Starsick
 
 \x1b[34mHack Friendlist Facebook
 \x1b[00mAutomatic cracking password with Bruteforce
